97d575110956af5889a9c5036773cc0f41ca271a
AntonyKo <a8252525@gmail.com>
2019-11-05 18:10:58 +0800

    Binding with IoTtalk

--- Remote_control_example/app.py
+++ Remote_control_example/app.py
@@ -3,8 +3,10 @@ from flask import render_template
 from flask import make_response
 import csmapi
 
-app = Flask(__name__)
+
 csmapi.ENDPOINT  = "https://5.iottalk.tw"
+registered_address = '4655444565454465cscss'
+app = Flask(__name__)
 
 @app.route('/<mac_addr>/<count>/', methods=['GET', 'POST'])
 def SwitchSetCount(mac_addr, count):
@@ -29,13 +31,13 @@ def SwitchSetCount(mac_addr, count):
             if i == '1':
                 s_count+=1
         print(s_count)
-        return make_response(render_template('SwitchSet.html', mac_addr=registered_address, count=int(s_count)))
+        return make_response(render_template('index.html', mac_addr=registered_address, count=int(s_count)))
 
     except Exception as e:
         print(e)
-    return make_response(render_template('SwitchSet.html', mac_addr=registered_address, count=int(count)))
+    return make_response(render_template('index.html', mac_addr=registered_address, count=int(count)))
 
     
 
 if __name__ == "__main__":
-    app.run('127.0.0.1', port=80, threaded=True, use_reloader=False)
+    app.run('127.0.0.1', port=9453, threaded=True, use_reloader=False, debug=True)
