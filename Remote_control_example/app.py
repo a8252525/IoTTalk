2143029b5af1b3339aa27769d2fe1f6721813260
AntonyKo <a8252525@gmail.com>
2019-11-06 13:34:38 +0800

    Button can change with IoTTalk. Switch still has problem

--- Remote_control_example/app.py
+++ Remote_control_example/app.py
@@ -22,16 +22,22 @@ def SwitchSetCount(mac_addr, count):
             print("Device feature list = ", device_feature_list)
 
         control_channel_output = csmapi.pull(registered_address, '__Ctl_O__') #Pull the Output of Control Channel
+        print(control_channel_output[0][1][1])
         if control_channel_output:
+            print(control_channel_output[0][1][1])
             selected_device_feature_flags = control_channel_output[0][1][1]['cmd_params'][0]
             print("Selected device feature flags = ", selected_device_feature_flags)
         print(type(selected_device_feature_flags))
         print('before_for')
-        for i in selected_device_feature_flags:
-            if i == '1':
-                s_count+=1
-        print(s_count)
-        return make_response(render_template('index.html', mac_addr=registered_address, count=int(s_count)))
+        keypad_count = selected_device_feature_flags[:9].count('1')
+        color_count = selected_device_feature_flags[9:18].count('1')
+        button_count = selected_device_feature_flags[18:27].count('1')
+        switch_count = selected_device_feature_flags[27:36].count('1')
+        knob_count = selected_device_feature_flags[36:45].count('1')
+        print(keypad_count,color_count,button_count,switch_count,knob_count)
+
+
+        return make_response(render_template('index.html', mac_addr=registered_address, keypad_count=keypad_count,color_count=color_count , button_count = button_count, switch_count = switch_count , knob_count = knob_count))
 
     except Exception as e:
         print(e)
